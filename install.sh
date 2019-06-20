81e58b76e5ab3d1240424f5eca85afcf7c20e1d2
Jing Liang <jing@1strategy.com>
2019-06-20 11:25:58 -0700

    update packer config

---
+++ install.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+sudo yum -y install httpd
+sudo sh -c "echo '<html><h1>Hello from Amazon EC2</h1></html>' > /var/www/html/index.html"
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
