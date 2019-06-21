e4b663168064c9dcbc25f715416763bdc8f03ab0
Jing Liang <jing@1strategy.com>
2019-06-20 20:02:57 -0700

    test v2

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 sudo yum -y install httpd
-sudo sh -c "echo '<html><h1>Hello from Amazon EC2</h1></html>' > /var/www/html/index.html"
+sudo sh -c "echo '<html><h1>Hello from Amazon EC2, Version 2.0</h1></html>' > /var/www/html/index.html"
 sudo systemctl enable httpd.service
 sudo systemctl start httpd.service
